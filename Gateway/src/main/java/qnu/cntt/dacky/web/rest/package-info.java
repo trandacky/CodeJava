b866eaef689ecf90c8ca8fb635df92316c536340
DacKy <trandackydbz2@gmail.com>
2021-01-28 19:30:37 +0700

    Initial version of Gateway generated by JHipster-6.10.5

---
+++ Gateway/src/main/java/qnu/cntt/dacky/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package qnu.cntt.dacky.web.rest;
---
+++ Gateway/src/main/java/qnu/cntt/dacky/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package qnu.cntt.dacky.web.rest.vm;
