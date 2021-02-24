35c92cef457fb5821f72090ff4df8e921a0b575d
DacKy <trandackydbz2@gmail.com>
2021-02-24 19:27:25 +0700

    commit for Ribbon

--- consumer/src/main/java/com/dacky/controller/ConsumerControllerClient.java
+++ consumer/src/main/java/com/dacky/controller/ConsumerControllerClient.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,11 +17,13 @@ import org.springframework.web.client.RestTemplate;
 
 public class ConsumerControllerClient {
 	@Autowired
-	private DiscoveryClient discoveryClient;
-
+	private LoadBalancerClient loadBalancer;
+	
 	public void getEmployee() throws RestClientException, IOException {
-		List<ServiceInstance> instances = discoveryClient.getInstances("employee-producer");
-		ServiceInstance serviceInstance = instances.get(0);
+		
+		ServiceInstance serviceInstance=loadBalancer.choose("employee-producer");
+		
+		System.out.println(serviceInstance.getUri());
 
 		String baseUrl = serviceInstance.getUri().toString();
 		baseUrl = baseUrl + "/employee";
