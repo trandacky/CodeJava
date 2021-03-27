f0c471b613a3124ad61d60eb8207653162f8efa0
DacKy <trandackydbz2@gmail.com>
2021-03-27 16:03:17 +0700

    Initial version of someservice generated by JHipster-6.10.5

---
+++ someservice/src/main/java/qnu/cntt/dacky/config/Constants.java
@@ -0,0 +1,12 @@
+package qnu.cntt.dacky.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
